705d3cfb32a402b3a23b3f47ad0d51e1ac835919
Łukasz Falaciński <lfalacinski@developico.com>
2025-09-08 08:36:44 +0200

    style(dark): unify calendar & projects backgrounds with dashboard card tone

--- components/projects/projects-table.tsx
+++ components/projects/projects-table.tsx
@@ -118,7 +118,7 @@ export function ProjectsTable() {
           </div>
         </CardHeader>
         <CardContent>
-          <div className="rounded-md border bg-card/70">
+          <div className="rounded-md border bg-card">
             <Table>
               <TableHeader>
                 <TableRow>
